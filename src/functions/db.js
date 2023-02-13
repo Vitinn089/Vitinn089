f49b07cece3195c2a25f79b522f4f95e509b1bc5
Vitinn089 <vitorlopes089@gmail.com>
2023-02-12 21:22:33 -0300

    feat: Criado estrutura do site pra exibição dos cards na home page para exibição dos projetos.

---
+++ src/functions/db.js
@@ -0,0 +1,39 @@
+const project = [
+	{
+		name: 'devfinance',
+		title: 'devFinance',
+		display: true,
+		capaUrl: '/images/capa-devfinance.png',
+		imageUrl: '/images/inicio-devfinance.png',
+	},
+	{
+		name: 'feedget-web',
+		title: 'Feedget',
+		display: true,
+		capaUrl: '/images/capa-feedget.png',
+		imageUrl: '/images/inicio-feedget.png',
+	},
+	{
+		name: 'nlw2',
+		title: 'Proffys',
+		display: true,
+		capaUrl: '/images/capa-proffy.png',
+		imageUrl: '/images/inicio-proffy.png',
+	},
+	{
+		name: 'bossfoot',
+		title: 'Boosfoot',
+		display: false,
+		// capaUrl: '/images/capa-bossfoot.png',
+		// imageUrl: '/images/inicio-bossfoot.png',
+	},
+	{
+		name: 'Vitinn089',
+		title: 'Vitinn',
+		display: false,
+		// capaUrl: '/images/capa-Vitinn.png',
+		// imageUrl: '/images/inicio-Vitinn.png',
+	}
+];
+
+export default project;
--- src/routes/home.js
+++ src/routes/home.js
@@ -1,11 +1,15 @@
 import express from 'express';
+import projectsImgs from '../functions/db.js';
 const router = express.Router();
+
 
 router.get('/', async (req, res) => {
 	const perfil = await fetch(process.env.URL_GITHUB).then(data => data.json());
+	const projects = await fetch(perfil.repos_url).then(data => data.json());
+
     
 	res.status(200);
-	res.render('pages/index', {avatar: perfil.avatar_url});
+	res.render('pages/index', {avatar: perfil.avatar_url, projects, projectsImgs});
 });
 
 export default router;
