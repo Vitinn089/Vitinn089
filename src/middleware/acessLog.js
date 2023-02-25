a9c2af15bcfbcd40aa030d8fbb3df5ca84196f26
Vitinn089 <vitorlopes089@gmail.com>
2023-02-25 20:57:13 -0300

    refactor: Code refactor.

--- src/middleware/acessLog.js
+++ src/middleware/acessLog.js
@@ -2,6 +2,6 @@
 export default function acessLog (req, res, next) {
 	const timeElapsed = Date.now();
 	const time = new Date(timeElapsed);
-	console.log(`${time.toLocaleString('pt-Br')}\t${req.path}`);
+	console.log(`${time.toLocaleString('pt-Br')}\t${req.path}\t${req.connection.remoteAddress}`);
 	next();
 }
