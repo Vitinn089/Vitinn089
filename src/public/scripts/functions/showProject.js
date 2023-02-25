8d2de7f25c8a02cb61a99fd58cea3eb470d47644
Vitinn089 <vitorlopes089@gmail.com>
2023-02-25 18:05:43 -0300

    feat: Add dynamic text in the technologies section of the project view frame.

--- src/public/scripts/functions/showProject.js
+++ src/public/scripts/functions/showProject.js
@@ -3,15 +3,11 @@ export default function showProject () {
 	const projectSelected = cardSelected.getAttribute('data-project');
 	const currentProject = JSON.parse(sessionStorage.repos).filter(project => projectSelected == project.name ? true : false)[0];
 
-
-	const languageMsg = Object.keys(currentProject.languages).reduce((text, lang, i, a) => {
-		i == (a.length-1) ?  text += lang : text += lang + ' // ';
-		return text;
-	}, '');
-
 	document.querySelector('.board-header img').setAttribute('src', `/images/inicio-${currentProject.name}.png`);
 	document.querySelector('.board-header-content h2').textContent = currentProject.title;
-	document.querySelector('.board-header-content p').textContent = languageMsg;
+	document.querySelector('.board-header-content p').textContent = currentProject.languages;
+	document.querySelector('.board-header').setAttribute('href', currentProject.url);
 	document.querySelector('.board-content p').textContent = currentProject.description;
-	// let ul = document.querySelector('.desc-content ul');
+	document.querySelector('.board-content ul').innerHTML = '';
+	currentProject.topics.forEach(topic => document.querySelector('.board-content ul').innerHTML += `<li><span>${topic.toUpperCase()}</span></li>`);
 }
